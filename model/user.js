0f59776a2a8220e2331894279ee71bb7e54f214e
Elliot <elliot@codeaura.ai>
2024-06-06 13:05:52 +0100

    Implement password hashing in the user model using bcrypt.

--- model/user.js
+++ model/user.js
@@ -18,5 +18,19 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
+// Password hashing middleware
+UserSchema.pre('save', async function(next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
 // Export the User model
 module.exports = mongoose.model('User', UserSchema);
