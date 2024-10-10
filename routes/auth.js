a5842e2684d779bff9121d581c4b2a88fa46de98
Elliot <elliot@codeaura.ai>
2024-10-10 06:15:09 +0100

    Ajouté une méthode pour changer le mot de passe de l'utilisateur dans le modèle. Créé le contrôleur pour le changement de mot de passe. Ajouté une route pour le changement de mot de passe.

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { login } = require('../controller/auth-controller');
+const { changePassword } = require('../controller/password-change-controller');
 
 // POST /auth/login
 router.post('/login', login);
+
+// POST /auth/change-password
+router.post('/change-password', changePassword);
 
 module.exports = router;
